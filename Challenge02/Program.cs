12eb5bee88bdea6ba1a912e828fa93329c41b75f
matthewtoghill <matthew.toghill@outlook.com>
2022-11-19 23:52:28 +0000

    Complete C3

--- Challenge02/Program.cs
+++ Challenge02/Program.cs
@@ -5,23 +5,34 @@ namespace Challenge02;
 public static class Program
 {
     private static readonly string _input = File.ReadAllText(@"..\..\..\..\Data\challenge02.txt");
+    private static readonly string _mail = "73 107110111119 121111117 121111117 100111 110111116 107110111119 109101 73 97109 1199711699104105110103 121111117 73 97109 102111108108111119105110103 121111117 68111 121111117 11997110116 116111 11210897121 8010897121 119105116104 109101 79107 7610111639115 11210897121 82117110 116104105115 9911110910997110100 11511798109105116 116561181061045651505752561029911097108";
+
     private static void Main()
     {
-        var message = new StringBuilder();
+        var message = TranslateMessage(_input);
+        Console.WriteLine(message);
 
-        for (int i = 0; i < _input.Length; i++)
+        var mail = TranslateMessage(_mail);
+        Console.WriteLine(mail);
+    }
+
+    private static string TranslateMessage(string message)
+    {
+        var result = new StringBuilder();
+
+        for (int i = 0; i < message.Length; i++)
         {
-            var len = _input[i] switch
+            var len = message[i] switch
             {
-                '8' or '9' => 2,
+                '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' => 2,
                 '1' => 3,
                 _ => 1
             };
 
-            message.Append(len == 1 ? ' ' : (char)int.Parse(_input[i..(i + len)]));
+            result.Append(len == 1 ? ' ' : (char)int.Parse(message[i..(i + len)]));
             i += len - 1;
         }
 
-        Console.WriteLine(message);
+        return result.ToString();
     }
 }
---
+++ Challenge03/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Challenge03;
+
+public class Program
+{
+    private static readonly string[] _input = File.ReadAllText(@"..\..\..\..\Data\challenge03.txt").Trim().Replace("'", "")
+                                                  .Split(new[] { "\n", ", ", "[", "]", " " }, StringSplitOptions.RemoveEmptyEntries);
+    private static void Main()
+    {
+        var zebras = new List<(string colour, int length)>();
+        for (int i = 0; i < _input.Length - 1; i++)
+        {
+            var colour1 = _input[i];
+            var colour2 = _input[i + 1];
+            var count = 1;
+            bool isSecondary = true;
+
+            for (int j = i + 1; j < _input.Length; j++)
+            {
+                var thisColour = _input[j];
+                if (isSecondary && thisColour != colour2 || !isSecondary && thisColour != colour1)
+                {
+                    zebras.Add((_input[j - 1], count));
+                    break;
+                }
+
+                count++;
+                isSecondary = !isSecondary;
+            }
+        }
+
+        var longest = zebras.MaxBy(x => x.length);
+        Console.WriteLine($"{longest.length}@{longest.colour}");
+    }
+}
