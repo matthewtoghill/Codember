d503ff55e71a8d2dd06d2a3ee935d3741270cfa6
matthewtoghill <matthew.toghill@outlook.com>
2022-11-22 20:42:17 +0000

    Complete C4

---
+++ Challenge04/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Challenge04;
+
+public class Program
+{
+    private static void Main()
+    {
+        var validPasswords = Enumerable.Range(11098, 98124 - 11098).Where(x => IsValid(x.ToString())).ToArray();
+        Console.WriteLine($"{validPasswords.Length}-{validPasswords[55]}");
+    }
+
+    private static bool IsValid(string s)
+        => s.Count(c => c == '5') >= 2
+        && s.Zip(s.Skip(1), (a, b) => a <= b).All(x => x);
+}
