7c0e2a9342a67837540a3771d9641ff88618a85b
matthewtoghill <matthew.toghill@outlook.com>
2022-11-29 18:19:03 +0000

    Complete C5

---
+++ Challenge05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace Challenge05;
+
+public class Program
+{
+    private static readonly string[] _input = JsonSerializer.Deserialize<string[]>(File.ReadAllText(@"..\..\..\..\Data\challenge05.json"))!;
+    private static void Main()
+    {
+        var patrons = _input.Select((x, i) => (name: x, index: i)).ToList();
+
+        while (patrons.Count > 1)
+        {
+            for (int i = 0; i < patrons.Count; i++)
+                patrons.RemoveAt((i + 1) % patrons.Count);
+        }
+
+        Console.WriteLine($"{patrons[0].name}-{patrons[0].index}");
+    }
+}
