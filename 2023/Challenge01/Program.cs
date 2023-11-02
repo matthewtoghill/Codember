742552e9455c6c9a1442392119eefea3839ff67b
matthewtoghill <matthew.toghill@outlook.com>
2023-11-02 18:37:20 +0000

    Add 2023 challenge 1
    and reorganise 2022 challenges

---
+++ 2023/Challenge01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Challenge01;
+
+public class Program
+{
+    private static readonly string[] _input = File.ReadAllText(@"..\..\..\..\Data\challenge01.txt").Split(' ');
+    private static void Main()
+    {
+        foreach (var item in _input.GetFrequencies())
+        {
+            Console.Write($"{item.Key}{item.Value}");
+        }
+    }
+}
+
+
+public static class Extensions
+{
+    public static IEnumerable<KeyValuePair<T, int>> GetFrequencies<T>(this IEnumerable<T> items) where T : notnull
+        => items.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+}
