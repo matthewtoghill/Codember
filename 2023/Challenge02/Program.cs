bc1369c8e326bb91c8de397b52a1a9a638fa95dc
matthewtoghill <matthew.toghill@outlook.com>
2023-11-11 11:10:16 +0000

    Add 2023 challenge 2

---
+++ 2023/Challenge02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Challenge02;
+
+public class Program
+{
+    private static readonly string _input = File.ReadAllText(@"..\..\..\..\Data\challenge02.txt");
+    private static void Main()
+    {
+        var value = 0;
+        _input.ToList().ForEach(c => value = GetOperation(c).Invoke(value));
+    }
+
+    public static Func<int, int> GetOperation(char command) =>
+        command switch
+        {
+            '#' => x => x + 1,
+            '@' => x => x - 1,
+            '*' => x => x * x,
+            '&' => x => { Console.Write(x); return x; }
+        };
+}
+
