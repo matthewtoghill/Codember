5c1021ea501620f2d0a99a7e3b168f7f86e3b290
matthewtoghill <matthew.toghill@outlook.com>
2023-11-21 20:32:57 +0000

    Add 2023 challenge 3

---
+++ 2023/Challenge03/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Challenge03;
+
+public class Program
+{
+    private static readonly string[] _input = File.ReadAllLines(@"..\..\..\..\Data\challenge03.txt");
+    private static void Main()
+    {
+        Console.WriteLine($"{_input.Where(IsPasswordInvalid).ToArray()[41]}");
+    }
+
+    private static bool IsPasswordInvalid(string password)
+    {
+        var split = password.Split(["-", ":", " "], StringSplitOptions.RemoveEmptyEntries);
+        var (min, max) = (int.Parse(split[0]), int.Parse(split[1]));
+        var count = split[3].Count(c => c == split[2][0]);
+        return !(min <= count && count <= max);
+    }
+}
